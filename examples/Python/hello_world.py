db0b1d082af032d8bd2e32fd4afafcb91e000257
Goinwrong <151388101+Goinwrong@users.noreply.github.com>
2024-12-25 21:58:34 -0500

    Create hello_world.py

---
+++ examples/Python/hello_world.py
@@ -0,0 +1,12 @@
+# hello_world.py
+
+# Welcome to the Hello World Tour!
+# This is the Python contribution to our global project.
+
+def main():
+    """Prints a friendly Hello World message."""
+    print("Hello, World! 🌍")
+    print("Welcome to the Hello World Tour!")
+
+if __name__ == "__main__":
+    main()
